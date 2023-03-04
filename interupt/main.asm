c4595052f4622e59ed5d082262c7a62972ceb809
egormeister02 <116089772+egormeister02@users.noreply.github.com>
2023-03-04 21:08:30 +0300

    add ramka

--- interupt/main.asm
+++ interupt/main.asm
@@ -13,12 +13,19 @@ New09               proc
                     xor ax, ax
                     in al, 60h
 
-                    cmp al, 29d
-                    je @@del08
-                    cmp al, 41d
+                    ;mov dx, 2805h
+                    ;call PrintDec
+                    ;jmp @@skip
+
+                    cmp al, 169d                    ; code of '~' up
+                    je @@skip2
+                    cmp al, 41d                     ; code of '~' down
                     jne @@skip                    
 
-                    cli
+                    cmp flag, 0
+                    jne @@del08
+                    mov flag, 1
+                    cli                             ; add new 08 interrupt
                     xor ax, ax
                     mov es, ax
                     mov bx, 4d * 8d                    
@@ -28,7 +35,8 @@ New09               proc
                     sti                    
                     jmp @@skip2
 
-@@del08:            cli
+@@del08:            mov flag, 0
+                    cli                             ; return old 08 interrupt
                     xor ax, ax
                     mov es, ax
                     mov bx, 4d * 8d  
@@ -39,15 +47,15 @@ New09               proc
                     sti  
                     jmp @@skip2                
                     
-@@skip:             pop ds es dx cx bx ax
+@@skip:             pop ds es dx cx bx ax           ; end with call old 09 interrupt
                     db 0eah
                     old09fun dw 0
                     old09seg dw 0
                     iret
                     
 
-@@skip2:            in al, 61h
-                    or al, 80h
+@@skip2:            in al, 61h                      ; end without call old 09 interrupt
+                    or al, 80h                      ; talk with PPI and INTC
                     out 61h, al
                     and al, not 80h
                     out 61h, al
@@ -57,18 +65,48 @@ New09               proc
                     iret
                     endp
 
+flag                db 0
+
 New08               proc
-                    push ax bx cx dx es ds
+                    push si ds es dx cx bx ax
                     
                     mov bx, cs
                     mov ds, bx
+                    mov bl, 02h
+                    mov dx, 3a04h
+                    mov ax, 0c06h
+                    
+                    call Ramka
+                    mov bx, sp
+                    mov ax, ss:[bx]
                     mov dx, 4005h
                     call PrintDec
-                    mov dx, 3B05h
+                    mov dx, 3b05h
                     mov ax, offset StrAx
                     call PrintStr
+
+                    mov ax, ss:[bx+2]
+                    mov dx, 4006h
+                    call PrintDec
+                    mov dx, 3b06h
+                    mov ax, offset StrBx
+                    call PrintStr
+
+                    mov ax, ss:[bx+4]
+                    mov dx, 4007h
+                    call PrintDec
+                    mov dx, 3b07h
+                    mov ax, offset StrCx
+                    call PrintStr
+
+                    mov ax, ss:[bx+6]
+                    mov dx, 4008h
+                    call PrintDec
+                    mov dx, 3b08h
+                    mov ax, offset StrDx
+                    call PrintStr
                                         
-                    pop ds es dx cx bx ax
+                    pop ax bx cx dx es ds si
                     db 0eah
                     old08fun dw 0
                     old08seg dw 0
@@ -157,8 +195,109 @@ PrintDec            proc
                     ret
                     endp
 
+;-----------------------------------------
+;Print ramka
+;-----------------------------------------
+;Entry:             AH = length
+;                   AL = hight
+;                   DH = X-coordinate
+;                   DL = Y-coordinate
+;                   BL = code of color
+;
+;Exit:              None
+;
+;Expects:           None
+;
+;Destroys:          AX BX CX DX ES SI
+;-----------------------------------------
+
+Ramka               proc
+                    mov si, bx
+                    mov bx, 0b800h        ; video mem seg addr
+                    mov es, bx
+                    
+                    mov cx, ax            ; transition to coordinates
+                    mov al, 80d
+                    mul dl
+                    shr dx, 8
+                    add ax, dx 
+                    shl ax, 1
+                    mov bx, ax
+                    mov ax, cx                    
+
+                    xor dx, dx
+                    mov dl, al
+                    shr ax, 8
+
+                    mov cx, si
+                    mov si, ax
+                    mov ax, cx
+                    xor cx, cx
+                    
+                    mov byte ptr es:[bx], 00c9h
+                    mov byte ptr es:[bx+1], al
+                    mov cx, si
+                    add cx, -2
+
+@@Next1:            add bx, 2
+                    mov byte ptr es:[bx], 00cdh
+                    mov byte ptr es:[bx+1], al
+                    loop @@Next1
+
+                    add bx, 2
+                    mov byte ptr es:[bx], 00bbh
+                    mov byte ptr es:[bx+1], al
+
+                    sub bx, si
+                    sub bx, si
+                    add bx, 2
+                    add bx, 160d
+
+                    mov cl, dl
+                    add cl, -2 
+@@NewStr:
+                    push cx
+                    mov byte ptr es:[bx], 00bah
+                    mov byte ptr es:[bx+1], al
+                    
+                    mov cx, si
+                    add cx, -1
+
+@@NextIn:           add bx, 2
+                    mov byte ptr es:[bx], 0020h
+                    mov byte ptr es:[bx+1], al
+                    loop @@NextIn
+
+                    mov byte ptr es:[bx], 00bah
+                    mov byte ptr es:[bx+1], al
+                    sub bx, si
+                    sub bx, si
+                    add bx, 162d
+                    pop cx
+                    loop @@NewStr
+
+                    mov byte ptr es:[bx], 00c8h
+                    mov byte ptr es:[bx+1], al
+                    mov cx, si
+                    add cx, -2
+
+@@Next2:            add bx, 2
+                    mov byte ptr es:[bx], 00cdh
+                    mov byte ptr es:[bx+1], al
+                    loop @@Next2
+
+                    add bx, 2
+                    mov byte ptr es:[bx], 00bch
+                    mov byte ptr es:[bx+1], al
+
+                    ret
+                    endp
+
 divider             dw 0ah
 StrAx               db 'AX = ', 0
+StrBx               db 'BX = ', 0
+StrCx               db 'CX = ', 0
+StrDx               db 'DX = ', 0
 EOP:                
                     cli
                     xor ax, ax
